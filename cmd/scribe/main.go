5760bf334d80a066d7068e02a2ede933be7857b3
Andrew Hare <andrew@hares.co>
2020-09-29 17:05:13 -0600

    Use lorca for now

--- cmd/scribe/main.go
+++ cmd/scribe/main.go
@@ -1,100 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/url"
 
-	"fyne.io/fyne"
-	"fyne.io/fyne/app"
-	"fyne.io/fyne/layout"
-	"fyne.io/fyne/theme"
-	"fyne.io/fyne/widget"
-
-	"github.com/fyne-io/examples/img/icon"
+	"github.com/zserge/lorca"
 )
 
-type textEdit struct {
-	cursorRow, cursorCol *widget.Label
-	entry                *widget.Entry
-	window               fyne.Window
-}
-
-func (e *textEdit) updateStatus() {
-	e.cursorRow.SetText(fmt.Sprintf("%d", e.entry.CursorRow+1))
-	e.cursorCol.SetText(fmt.Sprintf("%d", e.entry.CursorColumn+1))
-}
-
-func (e *textEdit) cut() {
-	e.entry.TypedShortcut(&fyne.ShortcutCut{Clipboard: e.window.Clipboard()})
-}
-
-func (e *textEdit) copy() {
-	e.entry.TypedShortcut(&fyne.ShortcutCopy{Clipboard: e.window.Clipboard()})
-}
-
-func (e *textEdit) paste() {
-	e.entry.TypedShortcut(&fyne.ShortcutPaste{Clipboard: e.window.Clipboard()})
-}
-
-func (e *textEdit) buildToolbar() *widget.Toolbar {
-	return widget.NewToolbar(widget.NewToolbarAction(theme.DocumentCreateIcon(), func() {
-		e.entry.SetText("")
-	}),
-		widget.NewToolbarSeparator(),
-		widget.NewToolbarAction(theme.ContentCutIcon(), func() {
-			e.cut()
-		}),
-		widget.NewToolbarAction(theme.ContentCopyIcon(), func() {
-			e.copy()
-		}),
-		widget.NewToolbarAction(theme.ContentPasteIcon(), func() {
-			e.paste()
-		}))
-}
+const html = `
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>scribe</title>
+		<style>
+			div#display {
+				border: 1px solid #999;
+				padding: 5px;
+				margin-bottom: 5px;
+				height: 93vh;
+			}
+		</style>
+	</head>
+	<body>
+		<div contenteditable="true" id="display"></div>
+		<button id="save">Save Content</button>
+	</body>
+	<script>
+		document.getElementById("save").addEventListener("click", function() {
+			alert("hello, world");
+		});
+	</script>
+</html>
+`
 
 func main() {
-	var (
-		a      = app.New()
-		window = a.NewWindow("scribe")
-	)
-
-	window.SetIcon(icon.TextEditorBitmap)
-
-	entry := widget.NewMultiLineEntry()
-	cursorRow := widget.NewLabel("1")
-	cursorCol := widget.NewLabel("1")
-
-	editor := &textEdit{
-		cursorRow: cursorRow,
-		cursorCol: cursorCol,
-		entry:     entry,
-		window:    window,
+	ui, err := lorca.New("data:text/html,"+url.PathEscape(html), "", 600, 800)
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	toolbar := editor.buildToolbar()
-	status := widget.NewHBox(layout.NewSpacer(),
-		widget.NewLabel("Cursor Row:"), cursorRow,
-		widget.NewLabel("Col:"), cursorCol)
-	content := fyne.NewContainerWithLayout(layout.NewBorderLayout(toolbar, status, nil, nil),
-		toolbar, status, widget.NewScrollContainer(entry))
-
-	editor.entry.OnCursorChanged = func() {
-		editor.updateStatus()
-	}
-
-	window.SetMainMenu(fyne.NewMainMenu(
-		fyne.NewMenu("File",
-			fyne.NewMenuItem("New", func() {
-				editor.entry.SetText("")
-			}),
-		),
-		fyne.NewMenu("Edit",
-			fyne.NewMenuItem("Cut", editor.cut),
-			fyne.NewMenuItem("Copy", editor.copy),
-			fyne.NewMenuItem("Paste", editor.paste),
-		),
-	))
-
-	window.SetContent(content)
-	window.Resize(fyne.NewSize(480, 320))
-	window.ShowAndRun()
+	defer ui.Close()
+	// Wait until UI window is closed
+	<-ui.Done()
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/andrewhare/scribe
 
 go 1.14
 
-require (
-	fyne.io/fyne v1.3.3
-	github.com/fyne-io/examples v0.0.0-20200701085432-9d943a2d6ec9
-)
+require github.com/zserge/lorca v0.1.9
